8589c62808882c29b96b5fd8fbd8516745ccb566
17Atigogo_J <17Atigogo_J@bexleygs.co.uk>
2023-01-09 11:45:51 +0000

    Simple File Reading for acsii value

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,12 @@ public class Main {
     public static void simplesRead(){
         try {
             FileReader fr = new FileReader("input_file.txt");
-            int ascii = fr.read();
-            System.out.println(ascii);
+            int ascii = 0;
+            while (ascii != -1){
+                ascii = fr.read();
+                char letter = (char)ascii;
+                System.out.println(letter);
+            }
         }
         catch(IOException e) {
             e.printStackTrace();
@@ -15,6 +19,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	// write your code here
+	    simplesRead();
     }
 }
