0cf55abf06419836f83c7e8f31f63639a276bb56
17Atigogo_J <17Atigogo_J@bexleygs.co.uk>
2023-01-09 11:34:35 +0000

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,17 @@
 package com.company;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+    public static void simplesRead(){
+        try {
+            FileReader fr = new FileReader("input_file.txt");
+            int ascii = fr.read();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
 	// write your code here
